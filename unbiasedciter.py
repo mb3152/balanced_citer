eaa0932f08a05a80f451243c5c47b3d3bfadb5b0
Maxwell Bertolero <bertolero@berkeley.edu>
2020-09-02 12:56:50 -0400

    added gender

--- unbiasedciter.py
+++ unbiasedciter.py
@@ -2,20 +2,37 @@
 hi, this code is to check your citations for race biases
 please contact max bertolero at mbertolero@me.com for any questions!
 all you need to do is make sure you have the correct path to your bib file and where you store this repo
-then type something like:  %run unbiasedciter -authors 'Maxwell Bertolero Danielle Bassett' -bibfile '/Users/maxwell/Desktop/main.bib' 
+then type something like:  
+
+%run unbiasedciter -authors 'Maxwell Bertolero Danielle Bassett' -bibfile '/Users/maxwell/Desktop/main.bib' -gender_key 'key from gender-api.com'
+
 """
+
+gender_key = "RzYtabLemeouQQlJSY"
 import os
 import pandas as pd
 import tqdm
 import matplotlib.pylab as plt
 import matplotlib.gridspec as gridspec
 import numpy as np
+import json
+from urllib.request import urlopen
 """
 some uncommon libs 
 """
-from ethnicolr import census_ln, pred_census_ln,pred_wiki_name
-from pybtex.database import parse_file
+try: 
+	from ethnicolr import census_ln, pred_census_ln,pred_wiki_name
+except:
+	os.system('pip install ethnicolr')
+	from ethnicolr import census_ln, pred_census_ln,pred_wiki_name
+try:
+	from pybtex.database import parse_file
+except: 
+	os.system('pip install pybtex')
+	from pybtex.database import parse_file
+
 import seaborn as sns
+
 
 import argparse
 parser = argparse.ArgumentParser()
@@ -24,6 +41,7 @@ parser.add_argument('-homedir',action='store',dest='homedir',default='//Users/ma
 parser.add_argument('-method',action='store',dest='method',default='wiki')
 parser.add_argument('-authors',action='store',dest='authors')
 parser.add_argument('-font',action='store',dest='font',default='Palatino') # hey, we all have our favorite
+parser.add_argument('-myKey',action='store',dest='font',default=None) # hey, we all have our favorite
 r = parser.parse_args()
 locals().update(r.__dict__)
 bibfile = parse_file(bibfile)
@@ -35,20 +53,55 @@ wiki_2_race = {"Asian,GreaterEastAsian,EastAsian":'api', "Asian,GreaterEastAsian
 "GreaterEuropean,WestEuropean,Germanic":'white', "GreaterEuropean,WestEuropean,Hispanic":'hispanic',
 "GreaterEuropean,WestEuropean,Italian":'white', "GreaterEuropean,WestEuropean,Nordic":'white'}
 
-
-citation_matrix = np.zeros((4,4))
-small_matrix = np.zeros((2,2))
-matrix_idxs = {'white':0,'api':1,'hispanic':2,'black':3}
-small_idxs = {'white':0,'api':1,'hispanic':1,'black':1}
-
 authors = authors.split(' ')
-
 print ('first author is %s %s '%(authors[0],authors[1]))
 print ('last author is %s %s '%(authors[2],authors[2]))
 print ("we don't count these")
 
 
+"""
+for unknown gender, fill with base rates
+"""
+main_df = pd.read_csv('/%s/data/NewArticleData2019.csv'%(homedir),header=0)
+prs = np.zeros((main_df.shape[0],8,8))
+
+gender_base = {}
+for year in np.unique(main_df.PY.values):
+	ydf = main_df[main_df.PY==year].AG
+	fa = np.array([x[0] for x in ydf.values])
+	la = np.array([x[1] for x in ydf.values])
+
+	fa_m = len(fa[fa=='M'])/ len(fa[fa!='U'])
+	fa_w = len(fa[fa=='W'])/ len(fa[fa!='U'])
+
+	la_m = len(la[fa=='M'])/ len(la[la!='U'])
+	la_w = len(la[fa=='W'])/ len(la[la!='U'])
+
+	gender_base[year] = [fa_m,fa_w,la_m,la_w]
+
+gender_base[2020] = [fa_m,fa_w,la_m,la_w]
+
+
+citation_matrix = np.zeros((8,8))
+matrix_idxs = {'white_m':0,'api_m':1,'hispanic_m':2,'black_m':3,'white_f':4,'api_f':5,'hispanic_f':6,'black_f':7}
+
+asian = [0,1,2]
+black = [3,4]
+white = [5,6,7,8,9,11,12]
+hispanic = [10]
+
 for paper in tqdm.tqdm(bibfile.entries,total=len(bibfile.entries)): 
+	if 'author' not in bibfile.entries[paper].persons.keys():
+		continue #some editorials have no authors
+	
+	if 'year' not in bibfile.entries[paper].fields.keys():
+		year = 2020
+	else: year = int(bibfile.entries[paper].fields['year'])  
+	
+	if year not in gender_base.keys():
+		gb = gender_base[1995]
+	else:
+		gb = gender_base[year]
 	fa = bibfile.entries[paper].persons['author'][0]
 	try:fa_fname = fa.first_names[0] 
 	except:fa_fname = fa.last_names[0] #for people like Plato
@@ -74,68 +127,82 @@ for paper in tqdm.tqdm(bibfile.entries,total=len(bibfile.entries)):
 		if la_lname == authors[3]:
 			continue
 
-	##wiki
-	if method =='wiki':
-		names = [{'lname': fa_lname,'fname':fa_fname}]
-		fa_df = pd.DataFrame(names,columns=['fname','lname'])
-		fa_race = wiki_2_race[pred_wiki_name(fa_df,'fname','lname').race.values[0]]
-		names = [{'lname': la_lname,'fname':la_fname}]
-		la_df = pd.DataFrame(names,columns=['fname','lname'])
-		la_race = wiki_2_race[pred_wiki_name(la_df,'fname','lname').race.values[0]]
-	#census
-	if method =='census':
-		names = [{'name': fa_lname},{'name':la_lname}]
-		la_df = pd.DataFrame(names)
-		r = pred_census_ln(la_df,'name')
-		fa_race,la_race= r.race.values
-	citation_matrix[matrix_idxs[fa_race],matrix_idxs[la_race]] +=1
-	small_matrix[small_idxs[fa_race],small_idxs[la_race]] +=1
-# 1/0
+	fa_fname = fa_fname.encode("ascii", errors="ignore").decode() 
+	fa_lname = fa_lname.encode("ascii", errors="ignore").decode()
+	la_fname = la_fname.encode("ascii", errors="ignore").decode() 
+	la_fname = la_lname.encode("ascii", errors="ignore").decode() 
+
+	names = [{'lname': fa_lname,'fname':fa_fname}]
+	fa_df = pd.DataFrame(names,columns=['fname','lname'])
+	fa_race = pred_wiki_name(fa_df,'fname','lname').values[0][3:]
+	fa_race = [np.sum(fa_race[white]),np.sum(fa_race[asian]),np.sum(fa_race[hispanic]),np.sum(fa_race[black])]
+	
+	names = [{'lname': la_lname,'fname':la_fname}]
+	la_df = pd.DataFrame(names,columns=['fname','lname'])
+	la_race = pred_wiki_name(la_df,'fname','lname').values[0][3:]
+	la_race = [np.sum(la_race[white]),np.sum(la_race[asian]),np.sum(la_race[hispanic]),np.sum(la_race[black])]
+
+	url = "https://gender-api.com/get?key=" + gender_key + "&name=%s" %(fa_fname)
+	response = urlopen(url)
+	decoded = response.read().decode('utf-8')
+	fa_gender = json.loads(decoded)
+	if fa_gender['gender'] == 'female':
+		fa_g = [0,fa_gender['accuracy']/100.]
+	
+	if fa_gender['gender'] == 'male':
+		fa_g = [fa_gender['accuracy']/100.,0]
+	if fa_gender['gender'] == 'unknown':
+		fa_g = gb[:2]
+
+	url = "https://gender-api.com/get?key=" + gender_key + "&name=%s" %(la_fname)
+	response = urlopen(url)
+	decoded = response.read().decode('utf-8')
+	la_gender = json.loads(decoded)
+	if la_gender['gender'] == 'female':
+		la_g = [0,la_gender['accuracy']/100.]
+	
+	if la_gender['gender'] == 'male':
+		la_g = [la_gender['accuracy']/100.,0]
+
+	if la_gender['gender'] == 'unknown':
+		la_g = gb[2:]  
+
+	paper_matrix = np.zeros((2,8))
+	paper_matrix[0] = np.outer(fa_g,fa_race).flatten() 
+	paper_matrix[1] = np.outer(la_g,la_race).flatten() 
+
+	paper_matrix = np.outer(paper_matrix[0],paper_matrix[1]) 
+
+	citation_matrix = citation_matrix + paper_matrix
+
+
+cmap = sns.diverging_palette(220, 10, as_cmap=True)
+names = ['white_m','api_m','hispanic_m','black_m','white_w','api_w','hispanic_w','black_w']
 plt.close()
-sns.set(style='white',font=font)
-fig, axes = plt.subplots(ncols=2,nrows=2,figsize=(7.5,6))
-heat = sns.heatmap(citation_matrix,annot=True,ax=axes[0,0])
-axes[0,0].set_ylabel('first author',labelpad=0)  
-heat.set_yticklabels(['white','api','hispanic','black'],rotation=65)
-axes[0,0].set_xlabel('last author',labelpad=0)  
-heat.set_xticklabels(['white','api','hispanic','black']) 
-heat.set_title('# of citations')  
+sns.set(style='white',font='Palatino')
+fig, axes = plt.subplots(ncols=2,nrows=1,figsize=(7.5,4))
+axes = axes.flatten()
+plt.sca(axes[0])
+heat = sns.heatmap(np.around((citation_matrix/citation_matrix.sum())*100,2),annot=True,ax=axes[0],annot_kws={"size": 8},cmap=cmap,vmax=1,vmin=0)
+axes[0].set_ylabel('first author',labelpad=0)  
+heat.set_yticklabels(names,rotation=0)
+axes[0].set_xlabel('last author',labelpad=0)  
+heat.set_xticklabels(names,rotation=90) 
+heat.set_title('percentage of citations')  
 
-citation_matrix = citation_matrix / np.sum(citation_matrix) 
+citation_matrix_sum = citation_matrix / np.sum(citation_matrix) 
 
-expected = np.load('/%s/data/expected_matrix_%s.npy'%(homedir,method))
+expected = np.load('/%s/expected_matrix_%s.npy'%(homedir,method))
 expected = expected/np.sum(expected)
 
-percent_overunder = np.ceil( ((citation_matrix - expected) / expected)*100)
-
-heat = sns.heatmap((percent_overunder).astype(int),annot=True,ax=axes[0,1],fmt='g')
-axes[0,1].set_ylabel('first author',labelpad=0)  
-heat.set_yticklabels(['white','api','hispanic','black'],rotation=65)
-axes[0,1].set_xlabel('last author',labelpad=0)  
-heat.set_xticklabels(['white','api','hispanic','black']) 
+percent_overunder = np.ceil( ((citation_matrix_sum - expected) / expected)*100)
+plt.sca(axes[1])
+heat = sns.heatmap(np.around(percent_overunder,2),annot=True,ax=axes[1],fmt='g',annot_kws={"size": 8},vmax=50,vmin=-50,cmap=cmap)
+axes[1].set_ylabel('',labelpad=0)  
+heat.set_yticklabels([''])
+axes[1].set_xlabel('last author',labelpad=0)  
+heat.set_xticklabels(names,rotation=90) 
 heat.set_title('percentage over/under-citations')
-
-# 1/0
-heat = sns.heatmap(small_matrix,annot=True,ax=axes[1,0])
-axes[1,0].set_ylabel('first author',labelpad=0)  
-heat.set_yticklabels(['white','poc'])
-axes[1,0].set_xlabel('last author',labelpad=0)  
-heat.set_xticklabels(['white','poc'])
-
-small_matrix = small_matrix / np.sum(small_matrix) 
-
-expected = np.load('/%s/data/expected_small_matrix_%s.npy'%(homedir,method))
-expected = expected/np.sum(expected)
-
-percent_overunder = np.ceil( ((small_matrix - expected) / expected)*100)
-heat = sns.heatmap((percent_overunder).astype(int),annot=True,ax=axes[1,1],fmt='g')
-axes[1,1].set_ylabel('first author',labelpad=0)  
-heat.set_yticklabels(['white','poc'])
-axes[1,1].set_xlabel('last author',labelpad=0)  
-heat.set_xticklabels(['white','poc'])
-
-
-
 plt.tight_layout()
-# 1/0
+
 plt.savefig('race_citations.pdf')

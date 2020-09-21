acb41a27ffb0cef5375ba35bd7dc8964cdc786ec
Maxwell Bertolero <mbertolero@me.com>
2020-09-21 13:10:04 -0400

    added statement
    also saves paper level results

--- unbiasedciter.py
+++ unbiasedciter.py
@@ -35,7 +35,7 @@ import seaborn as sns
 
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument('-bibfile',action='store',dest='bibfile',default='main.bib')
+parser.add_argument('-bibfile',action='store',dest='bibfile',default='example.bib')
 parser.add_argument('-homedir',action='store',dest='homedir',default='//Users/maxwell/Documents/GitHub/unbiasedciter/')
 parser.add_argument('-method',action='store',dest='method',default='wiki')
 parser.add_argument('-authors',action='store',dest='authors')
@@ -43,6 +43,7 @@ parser.add_argument('-font',action='store',dest='font',default='Palatino') # hey
 parser.add_argument('-gender_key',action='store',dest='gender_key',default=None) # hey, we all have our favorite
 r = parser.parse_args()
 locals().update(r.__dict__)
+bibname = bibfile.split('.')[0]
 bibfile = parse_file(bibfile)
 
 wiki_2_race = {"Asian,GreaterEastAsian,EastAsian":'api', "Asian,GreaterEastAsian,Japanese":'api',
@@ -95,11 +96,19 @@ asian = [0,1,2]
 black = [3,4]
 white = [5,6,7,8,9,11,12]
 hispanic = [10]
+print ('looping through your references, predicting gender and race')
 
+columns=['Reference Key','Author','Gender','W','A']
+paper_df = pd.DataFrame(columns=columns)
+
+gender = []
+race = []
+
+
+idx = 0
 for paper in tqdm.tqdm(bibfile.entries,total=len(bibfile.entries)): 
 	if 'author' not in bibfile.entries[paper].persons.keys():
 		continue #some editorials have no authors
-	
 	if 'year' not in bibfile.entries[paper].fields.keys():
 		year = 2020
 	else: year = int(bibfile.entries[paper].fields['year'])  
@@ -108,10 +117,12 @@ for paper in tqdm.tqdm(bibfile.entries,total=len(bibfile.entries)):
 		gb = gender_base[1995]
 	else:
 		gb = gender_base[year]
+
 	fa = bibfile.entries[paper].persons['author'][0]
 	try:fa_fname = fa.first_names[0] 
 	except:fa_fname = fa.last_names[0] #for people like Plato
 	fa_lname = fa.last_names[0] 
+
 	la = bibfile.entries[paper].persons['author'][-1]
 	try:la_fname = la.first_names[0] 
 	except:la_fname = la.last_names[0] #for people like Plato
@@ -136,7 +147,7 @@ for paper in tqdm.tqdm(bibfile.entries,total=len(bibfile.entries)):
 	fa_fname = fa_fname.encode("ascii", errors="ignore").decode() 
 	fa_lname = fa_lname.encode("ascii", errors="ignore").decode()
 	la_fname = la_fname.encode("ascii", errors="ignore").decode() 
-	la_fname = la_lname.encode("ascii", errors="ignore").decode() 
+	la_lname = la_lname.encode("ascii", errors="ignore").decode() 
 
 	names = [{'lname': fa_lname,'fname':fa_fname}]
 	fa_df = pd.DataFrame(names,columns=['fname','lname'])
@@ -154,7 +165,6 @@ for paper in tqdm.tqdm(bibfile.entries,total=len(bibfile.entries)):
 	fa_gender = json.loads(decoded)
 	if fa_gender['gender'] == 'female':
 		fa_g = [0,fa_gender['accuracy']/100.]
-	
 	if fa_gender['gender'] == 'male':
 		fa_g = [fa_gender['accuracy']/100.,0]
 	if fa_gender['gender'] == 'unknown':
@@ -171,7 +181,27 @@ for paper in tqdm.tqdm(bibfile.entries,total=len(bibfile.entries)):
 		la_g = [la_gender['accuracy']/100.,0]
 
 	if la_gender['gender'] == 'unknown':
-		la_g = gb[2:]  
+		la_g = gb[2:] 
+	
+	fa_data = np.array([paper,'%s,%s'%(fa_fname,fa_lname),'%s,%s'%(fa_gender['gender'],fa_gender['accuracy']),fa_race[0],np.sum(fa_race[1:])]).reshape(1,5)
+	paper_df = paper_df.append(pd.DataFrame(fa_data,columns=columns),ignore_index =True)
+	la_data = np.array([paper,'%s,%s'%(la_fname,la_lname),'%s,%s'%(la_gender['gender'],la_gender['accuracy']),la_race[0],np.sum(la_race[1:])]).reshape(1,5)
+	paper_df = paper_df.append(pd.DataFrame(la_data,columns=columns),ignore_index =True)
+
+	mm = fa_g[0]*la_g[0]
+	wm = fa_g[1]*la_g[0]
+	mw = fa_g[0]*la_g[1]
+	ww = fa_g[1]*la_g[1]
+	mm,wm,mw,ww = [mm,wm,mw,ww]/np.sum([mm,wm,mw,ww])
+	
+	gender.append([mm,wm,mw,ww])
+
+	ww = fa_race[0] * la_race[0]
+	aw = np.sum(fa_race[1:]) * la_race[0]
+	wa = fa_race[0] * np.sum(la_race[1:])
+	aa = np.sum(fa_race[1:]) * np.sum(la_race[1:])
+
+	race.append([ww,aw,wa,aa])
 
 	paper_matrix = np.zeros((2,8))
 	paper_matrix[0] = np.outer(fa_g,fa_race).flatten() 
@@ -180,6 +210,35 @@ for paper in tqdm.tqdm(bibfile.entries,total=len(bibfile.entries)):
 	paper_matrix = np.outer(paper_matrix[0],paper_matrix[1]) 
 
 	citation_matrix = citation_matrix + paper_matrix
+	idx = idx + 1
+
+mm,wm,mw,ww = np.mean(gender,axis=0)*100
+WW,aw,wa,aa = np.mean(race,axis=0)*100
+
+statement = "Recent work in several fields of science has identified a bias in citation practices such that papers from women and other minority scholars\
+are under-cited relative to the number of such papers in the field (1-5). Here we sought to proactively consider choosing references that reflect the \
+diversity of the field in thought, form of contribution, gender, race, ethnicity, and other factors. First, we obtained the predicted gender of the first \
+and last author of each reference by using databases that store the probability of a first name being carried by a woman (5, 6). By this measure \
+(and excluding self-citations to the first and last authors of our current paper), our references contain ww% woman(first)/woman(last), \
+MW% man/woman, WM% woman/man, and MM% man/man. This method is limited in that a) names, pronouns, and social media profiles used to construct the \
+databases may not, in every case, be indicative of gender identity and b) it cannot account for intersex, non-binary, or transgender people. \
+Second, we obtained predicted racial/ethnic category of the first and last author of each reference by databases that store the probability of a \
+first and last name being carried by an author of color (7,8). By this measure (and excluding self-citations), our references contain AA% author of \
+color (first)/author of color(last), WA% white author/author of color, AW% author of color/white author, and WW% white author/white author. This method \
+is limited in that a) names and Wikipedia profiles used to make the predictions may not be indicative of racial/ethnic identity, and b) \
+it cannot account for Indigenous and mixed-race authors, or those who may face differential biases due to the ambiguous racialization or ethnicization of their names.  \
+We look forward to future work that could help us to better understand how to support equitable practices in science."
+
+statement = statement.replace('MM',str(np.around(mm,2)))
+statement = statement.replace('WM',str(np.around(wm,2)))
+statement = statement.replace('MW',str(np.around(mw,2)))
+statement = statement.replace('ww',str(np.around(ww,2)))
+statement = statement.replace('WW',str(np.around(WW,2)))
+statement = statement.replace('AW',str(np.around(aw,2)))
+statement = statement.replace('WA',str(np.around(wa,2)))
+statement = statement.replace('AA',str(np.around(aa,2)))
+
+print (statement)
 
 
 cmap = sns.diverging_palette(220, 10, as_cmap=True)
@@ -211,5 +270,9 @@ heat.set_xticklabels(names,rotation=90)
 heat.set_title('percentage over/under-citations')
 plt.tight_layout()
 
-plt.savefig('/%s/data/race_gender_citations.pdf'%(homedir))
+plt.savefig('/%s/data/race_gender_citations_%s.pdf'%(homedir,bibname))
+
+
+
+
 
